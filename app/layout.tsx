7dd37d903f78e57ca97c74fba824863bec892830
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-09-04 15:52:31 +0100

    setting up environment #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
+
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
