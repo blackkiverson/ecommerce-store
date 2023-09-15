6e01c674ba16f0d72b2e326e142d995acf7a949b
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-09-15 18:52:28 +0100

    Individual Product Screen Setup.
    npm i @headlessui/react

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 const Footer = () => {
-    return ( 
-        // bg-bisque in-case of change.
-        <footer className="bg-bisque border-t">
+    return (
+        <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
                     &copy; 2023 FakeStoreNameA, Inc. All rights reserved.
