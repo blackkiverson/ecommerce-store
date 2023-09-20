ca1860ec8b2eb188ed72eebff7a9e5f239bd187b
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-09-20 09:52:31 +0100

    creating individual category screen #2

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -8,6 +8,7 @@ import Container from "@/components/ui/container";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 interface CategoryPageProps {
     params: {
@@ -38,8 +39,8 @@ const CategoryPage:React.FC<CategoryPageProps> = async ({
           <Billboard data={category.billboard} />
           <div className="px-4 sm:px-6 lg:px-8 pb-24">
             <div className="lg-grid lg:grid-cols-5 lg:gap-x-8">
-              {/* Mobile Filters */}
-              <div>
+              <MobileFilters sizes={sizes} colors={colors}/>
+              <div className="hidden lg:block">
                 <Filter valueKey="sizeId" name="Sizes" data={sizes} />
                 <Filter valueKey="colorId" name="Colors" data={colors} />
               </div>
