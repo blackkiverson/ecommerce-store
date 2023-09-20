96ac1eade3649f17381f24f94b3506572c48bce4
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-09-20 11:49:03 +0100

    Product Preview Modal Components
    npm i react-hot-toast
    
    Shopping Cart Functionality and Page
    
    npm install axios
    
    Cart Page Summary

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
@@ -16,7 +17,8 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
-  const previewModal = usePreviewModal();
+    const cart = useCart();
+    const previewModal = usePreviewModal();
     const router = useRouter();
 
     const handleClick = () => {
@@ -27,6 +29,12 @@ const ProductCard: React.FC<ProductCard> = ({
       event.stopPropagation();
 
       previewModal.onOpen(data);
+    }
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+      event.stopPropagation();
+
+      cart.addItem(data);
     }
 
     return (
@@ -46,6 +54,7 @@ const ProductCard: React.FC<ProductCard> = ({
                 icon={<Expand size={20} className="text-gray-600" />}
               />
               <IconButton
+              onClick={onAddToCart}
                 icon={<ShoppingCart size={20} className="text-gray-600" />}
               />
             </div>
